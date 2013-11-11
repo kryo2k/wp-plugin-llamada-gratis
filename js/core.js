07211f7db1e5dd02b5b73743f8a7b5ef083d1cd0
Hans Doller <kryo2k@gmail.com>
2013-11-11 14:54:11 -0600

    Added fixed width

--- js/core.js
+++ js/core.js
@@ -51,7 +51,7 @@
 			$w.remove();
 		};
 		$o = $('<div>').appendTo(root);
-		$w = $(['<div class="'+baseCls+'-window">',
+		$w = $(['<div class="'+baseCls+'-window fw360">',
 		 	'<div class="'+baseCls+'-wrapper">',
 		 		'<a class="'+baseCls+'-button"></a>',
 		 		'<h3 class="'+baseCls+'-title"></h3>',
